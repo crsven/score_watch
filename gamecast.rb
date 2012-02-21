831a96f7543bf7b9d3abf090c70afe5133721465
Chris Svenningsen <csvenningsen@leapfrogonline.com>
2012-02-21 14:40:58 -0600

    Initial Commit:
    Initial commit of Gamecast watcher. ARGV[0] should be a Gamecast game
    ID. Uses Nokogiri to parse Gamecast page every 30 seconds and await
    changes. When scoreline changes, pops a Growl notification.

---
+++ gamecast.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+
+require 'open-uri'
+require 'nokogiri'
+
+url = "http://soccernet.espn.go.com/gamecast?id="
+game = ARGV[0]
+url+=game
+
+old_score = "0-0"
+while game_on = TRUE do
+  html = Nokogiri::HTML(open(url))
+  scoreline = html.css(".matchup-score").first.content
+  scoreline.gsub("?","")
+  if scoreline != old_score
+    puts scoreline
+    growl_input = "-n 'Gamecast' -m 'Score was #{old_score}, but is now #{scoreline}'"
+    system("growlnotify #{growl_input}")
+    old_score = scoreline
+  end
+
+  time = html.css("##{game.to_s}clock").first.content
+  if time.gsub(" ","").gsub("-","").gsub("'","") == "90"
+    game_on = FALSE
+  end
+  sleep 30
+end
